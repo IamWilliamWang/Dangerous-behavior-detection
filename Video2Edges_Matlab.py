b6788527828bc58369a80ca7bbbeaa3541db7417
William Wang <iamjerichoholic@hotmail.com>
2019-04-07 14:56:20 +0800

    更新.m文件至1.4版本，更新调用接口

--- Video2Edges_Matlab.py
+++ Video2Edges_Matlab.py
@@ -3,4 +3,4 @@ import matlab.engine
 engine = matlab.engine.start_matlab()
 inputFile = '开关柜.mp4'
 outputFile = '输出.avi'
-engine.Video2Edges_Canny(inputFile, outputFile, nargout=0)
+engine.Video2Edges(inputFile, outputFile, nargout=0)
